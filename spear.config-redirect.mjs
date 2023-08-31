0dc1c43f25c20dae1008142e55d340a3c5e6c95c
Mantaroh YOSHINAGA <mantaroh@gmail.com>
2023-08-31 16:02:06 +0900

    redirect each generating page

--- spear.config-redirect.mjs
+++ spear.config-redirect.mjs
@@ -1,43 +1,8 @@
 import { parse } from 'node-html-parser';
 import { minify } from "html-minifier-terser";
+import { markdownPlugin } from "@spearly/spear-markdown-plugin";
 
-const dummyContent = {
-  id: "dummy",
-  type: "content",
-  values: {},
-  attributes: {
-      contentAlias: "sample",
-      createdAt: "",
-      nextContentId: null,
-      patternName: "a",
-      previousContentId: null,
-      publicUid: "sample",
-      publishedAt: "",
-      updatedAt: "",
-      fields: {
-          data: [
-            {
-              id: "sample",
-              type: "field",
-              attributes: {
-                  identifier: "dummy",
-                  inputType: "text",
-                  value: "dummy"
-              }
-            }
-          ]
-      }
-  }
-};
-
-const dummyList = {
-  totalContentsCount: 1,
-  matchingContentsCount: 1,
-  limit: 1,
-  offset: 0,
-  next: null,
-  data: [dummyContent]
-}
+const DEFAULT_FALLBACK_LANG = "en";
 
 export default {
   "projectName": "Spear document",
@@ -45,41 +10,20 @@ export default {
   "srcDir": ["./src"],
   "distDir": "./dist-redirect",
   "plugins": [
+    markdownPlugin({
+      directory: "data/jp",
+    }),
     {
       "pluginName": "redirect-plugin",
       "configuration": null,
-      "beforeBuild": async(state) => {
-        // Inject fake api client
-        state.jsGenerator.injectFakeApiClient({
-          analytics: {
-              pageView: () => {
-                  console.log("Unimplemented");
-                  return Promise.resolve();
-              },
-          },
-          getList: async () => {
-            return Promise.resolve(dummyList);
-          },
-          // Generate content from markdown files
-          getContent: async () => {
-            return Promise.resolve(dummyContent);
-          },
-          getContentPreview: () => {
-            return Promise.resolve(null);
-          },
-
-        })
-        return state
-      },
+      "beforeBuild": null,
       "afterBuild": async(state) => {
         try {
           for (const page of state.pagesList) {
             // 
             let html = await minify(page.rawData);
-            console.log("😺😺😺😺😺😺");
-            console.log(html);
                 // add redirect option to meta tag
-            const reidirectURL = `jp/${page.fname.replace(/^\//, "")}.html`;
+            const reidirectURL = `/${DEFAULT_FALLBACK_LANG}/${page.fname.replace(/^\//, "")}.html`;
             html = html.replace(/<\/head>/g, `<meta http-equiv="refresh" content="0; url=${reidirectURL}"></head>`);
             html = html.replace(/<body[^>]*>((.|[\n\r])*)<\/body>/,"");
             page.rawData = html;
